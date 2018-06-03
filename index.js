cb39c3d956620a66ac07e7a6142bba2fa170cb01
Anders Dahnielson <anders@dahnielson.com>
2018-06-03 18:23:23 +0200

    Abiding to JavaScript Standad Style

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Parcel = require('parcel-bundler');
-const path = require('path');
+const Parcel = require('parcel-bundler')
+const path = require('path')
 
-module.exports = plugin;
+module.exports = plugin
 
-function plugin(file, options) {
-    return function(files, metalsmith, done) {
-        const bundler = new Parcel(path.join(metalsmith.directory(), file), options)
-        bundler.bundle();
-        return done();
-    };
+function plugin (file, options) {
+  return function (files, metalsmith, done) {
+    const bundler = new Parcel(path.join(metalsmith.directory(), file), options)
+    bundler.bundle()
+    return done()
+  }
 }
