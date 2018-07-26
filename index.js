f7175a4b8b801fed2ad5a175dafc317c3c42a3e8
Giordano Piazza <gyopiazza@gmail.com>
2018-07-26 12:43:34 +0200

    Fix typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = plugin
 
 function plugin (file, options = {watch: false}) {
   return function (files, metalsmith, done) {
-    const bundler = new Parcel(file), options)
+    const bundler = new Parcel(file, options)
     bundler.bundle()
     return done()
   }
