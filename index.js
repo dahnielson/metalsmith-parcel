dc55551bea009cd50b7a0e491a319eec7d1848f7
Anders Dahnielson <anders@dahnielson.com>
2018-12-22 18:49:57 +0100

    Fixing spacing around curly braces

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Parcel = require('parcel-bundler')
 
 module.exports = plugin
 
-function plugin (file, options = {watch: false}) {
+function plugin (file, options = { watch: false }) {
   return function (files, metalsmith, done) {
     const bundler = new Parcel(file, options)
     bundler.bundle()
